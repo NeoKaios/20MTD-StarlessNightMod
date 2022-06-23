05a1f49901770bcb927361da253a3ac8890e7439
NeoKaios <mateo.dugas@student-cs.fr>
2022-06-23 18:20:03 +0200

    Add project files.

---
+++ StarlessNightMod/NightPatch.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using flanne.Core;
+using HarmonyLib;
+
+namespace StarlessNightMod
+{
+    public class NightPatch
+    {
+        [HarmonyPatch(typeof(InitState), "Enter")]
+        [HarmonyPostfix]
+        static void InitStateExit_postfix()
+        {
+            GameObject fogofwar = FindInSceneByName("FogOfWarCanvas");
+
+            if (fogofwar)
+            {
+                GameObject im = FindChildByName(fogofwar, "FogOfWarImage");
+
+                for (int i = 0; i < 5; i++)
+                {
+                    GameObject clone = GameObject.Instantiate(im);
+                    clone.transform.SetParent(fogofwar.transform);
+                }
+            }
+        }
+        static public GameObject FindChildByName(GameObject go, string name)
+        {
+
+            Transform[] transform = go.GetComponentsInChildren<Transform>();
+            foreach (Transform child in transform)
+            {
+                if (child.gameObject.name == name)
+                {
+                    return child.gameObject;
+                }
+            }
+            return null;
+        }
+        static public GameObject FindInSceneByName(string name)
+        {
+
+            GameObject[] gos = SceneManager.GetActiveScene().GetRootGameObjects();
+            foreach (GameObject go in gos)
+            {
+                GameObject res = FindChildByName(go, name);
+                if (res)
+                {
+                    return res;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ StarlessNightMod/StarlessNightPlugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace StarlessNightMod
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    class StarlessNightPlugin : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "kaios.mod.starless";
+        public const string PLUGIN_NAME = "Starless Night";
+        public const string PLUGIN_VERSION = "0.1";
+
+        public void Awake()
+        {
+            Logger.LogInfo("patch methods (NightPatch).");
+            try
+            {
+                Harmony.CreateAndPatchAll(typeof(NightPatch));
+            }
+            catch
+            {
+                Logger.LogError($"{PLUGIN_GUID} failed to patch methods (NightPatch).");
+            }
+        }
+    }
+}
