bed5e96bb9b0d3e82ee604a28a9f76506e20dd6a
NeoKaios <mateo.dugas@student-cs.fr>
2022-06-26 20:25:26 +0200

    Full project rework, from VS to VSCode

---
+++ src/StarlessNightPlugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace StarlessNightMod
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class StarlessNightPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            try
+            {
+                Harmony.CreateAndPatchAll(typeof(NightPatch));
+            }
+            catch
+            {
+                Logger.LogError($"{PluginInfo.PLUGIN_GUID} failed to patch methods (NightPatch).");
+            }
+        }
+    }
+}
